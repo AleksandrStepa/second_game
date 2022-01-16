5581f8dcf6a1de8b271617f5cf832c7cd59b2c41
Aleksandr Stepa <aleksandr.stepa@playgendary.com>
2022-01-16 18:24:36 +0300

    game completed

--- BalloonTap/main.lua
+++ BalloonTap/main.lua
@@ -3,11 +3,17 @@
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
+-- Переменная для хранения счета
+local tapCount = 0
 
 -- Загрузка фона и установка его в центр
 local background = display.newImageRect( "background.png", 360, 570 )
 background.x = display.contentCenterX
 background.y = display.contentCenterY
+
+-- Объект отображающий счет (колличество касаний мяча)
+local tapText = display.newText( tapCount, display.contentCenterX, 20, native.systemFont, 40 )
+tapText:setFillColor( 0.3, 0.6, 0 )
 
 -- Загрузка платформы и установка ее в центр и ниже на 25 пикселей
 local platform = display.newImageRect( "platform.png", 300, 50 )
@@ -27,3 +33,18 @@ physics.start()
 -- Конветирование изображений в физический объект
 physics.addBody( platform, "static" )
 physics.addBody( balloon, "dynamic", { radius=50, bounce=0.3 } )
+
+-- Функция касания мяча
+local function pushBalloon()
+  -- Метод применения импульса к объекту шар
+  balloon:applyLinearImpulse( 0, -0.75, balloon.x, balloon.y )
+
+  -- При касании мяча переменная увеличивается на 1
+  tapCount = tapCount + 1
+
+  -- В атрибут объекта tapText подставоляется обновленная переменная
+  tapText.text = tapCount
+end
+
+-- Когда проиходит касание, запускается функция pushBalloon
+balloon:addEventListener( "tap", pushBalloon )
